862dbcda2437af3b5c2fdf632a247a17a91b671d
mosen <admin@ebrosnan.local>
2011-10-10 14:44:10 +1100

    The files task supports globbing again. More utils.js test coverage.

--- test/utils.test.js
+++ test/utils.test.js
@@ -198,8 +198,9 @@ module.exports = {
         });
     },
 
+    // csslint (ASYNC)
 
-    'smoke test cssLint' : function(beforeExit, assert) {
+    'test cssLint with source file does not return an error' : function(beforeExit, assert) {
         utils.cssLint({
             sourceFile : './test/fixtures/test1.css'
         }, function(err, data) {
@@ -208,12 +209,92 @@ module.exports = {
         });
     },
 
-    'smoke test cssMinify' : function(beforeExit, assert) {
+    'test cssLint with source string does not return an error' : function(beforeExit, assert)  {
+        utils.cssLint({
+            source : 'root { display: block; }'
+        }, function(err, data) {
+            assert.ok(!err);
+            assert.isDefined(data);
+        });
+    },
+
+    'test cssLint with invalid source file returns an error' : function(beforeExit, assert) {
+        utils.cssLint({
+            sourceFile : './test/fixtures/invalid_dir/testabc.css'
+        }, function(err, data) {
+            assert.ok(err);
+        });
+    },
+
+    'test cssLint with no parameters returns an error' : function(beforeExit, assert) {
+        utils.cssLint({}, function(err, data) {
+            assert.ok(err);
+        });
+    },
+
+    // cssminify (ASYNC)
+
+    'test cssMinify with source file does not return an error' : function(beforeExit, assert) {
         utils.cssMinify({
             sourceFile : './test/fixtures/test1.css'
         }, function(err, data) {
             assert.ok(!err);
             assert.isDefined(data);
         });
+    },
+
+    'test cssMinify with source string does not return an error' : function(beforeExit, assert) {
+        utils.cssMinify({
+            source : 'root { display: block; }'
+        }, function(err, data) {
+            assert.ok(!err);
+            assert.isDefined(data);
+        });
+    },
+
+    'test cssMinify with invalid source file returns an error' : function(beforeExit, assert) {
+        utils.cssMinify({
+            sourceFile : './test/fixtures/invalid_dir/testabc.css'
+        }, function(err, data) {
+            assert.ok(err);
+        });
+    },
+
+//    'test cssMinify with unparseable css returns an error' : function(beforeExit, assert) {
+//        utils.cssMinify({
+//            source : 'root { \. isplay; __ //}'
+//        }, function(err, data) {
+////            console.log('This is the error' + err);
+////            console.log('This is the data ' + data);
+//            //assert.isDefined(err, 'Error is defined');
+////            assert.ok(err);
+//            //assert.ok(true);
+//        });
+//    },
+
+    'test cssMinify with destination file does not return an error' : function(beforeExit, assert) {
+        utils.cssMinify({
+            source : 'root { display: block; }',
+            destFile : './test/temp/cssminify1.css'
+        }, function(err, data) {
+            assert.ok(!err);
+            assert.ok(path.existsSync('./test/temp/cssminify1.css'));
+        });
+    },
+
+    'test cssMinify with invalid destination file returns an error' : function(beforeExit, assert) {
+        utils.cssMinify({
+            source : 'root { display: block; }',
+            destFile : './test/temp/invalid_dir/cssminifyabc.css'
+        }, function(err, data) {
+            assert.ok(err);
+        });
+    },
+
+    'test cssMinify with no parameters returns an error' : function(beforeExit, assert) {
+        utils.cssMinify({}, function(err, data) {
+            assert.ok(err);
+        });
     }
+
 };
