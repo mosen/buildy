0836123308c3d0b3b7f1d48c15d778a10fd388ab
mosen <admin@ebrosnan.local>
2011-10-12 16:39:43 +1100

    Created some tests for Cprf and Queue objects.

--- test/cprf.test.js
+++ test/cprf.test.js
@@ -1,58 +1,146 @@
 // Test the Cprf module
-var cprf = require('buildy/lib/cprf').cprf;
+var assert = require('assert'),
+    cprf = require('cprf'),
+    path = require('path'),
+    fs   = require('fs');
 
 
 module.exports = {
-    'test cprf copy source to destination file' : function(beforeExit, assert) {
+    'smoke test copy' : function(beforeExit, assert) {
+        var destFile = './test/temp/test1copy.css';
+        var copytest = cprf.copy('./test/fixtures/test1.css', './test/temp/test1copy.css', function(err) {
+            assert.ok(!err);
+        });
 
+        beforeExit(function() {
+           fs.unlinkSync(destFile);
+        });
     },
-    'test cprf exclusions list' : function(beforeExit, assert) {
 
+    'copy with non-existent source calls back with error' : function(beforeExit, assert) {
+        var destFile = './test/temp/test1copy.css';
+        var copytest = cprf.copy('./test/fixtures/test1abc.css', './test/temp/test1copy.css', function(err) {
+            assert.ok(err);
+        });
+
+        beforeExit(function() {
+           path.exists(destFile, function(exists) {
+                fs.unlinkSync(destFile);
+           });
+        });
     },
-    'test cprf recursive flag' : function(beforeExit, assert) {
 
+    'copy with directory as source calls back with error' : function(beforeExit, assert) {
+        var destFile = './test/temp/test1copy.css';
+        var copytest = cprf.copy('./test/fixtures', destFile, function(err) {
+            assert.ok(err);
+        });
+
+        beforeExit(function() {
+           path.exists(destFile, function(exists) {
+                fs.unlinkSync(destFile);
+           });
+        });
+    },
+
+    'copy with identical source and destination calls back with error' : function(beforeExit, assert) {
+        var testfile = './test/fixtures/test1.css';
+
+        var copytest = cprf.copy(testfile, testfile, function(err) {
+            assert.ok(err);
+        });
+    },
+
+    'copy with non existent destination directory and file creates destination directories' : function(beforeExit, assert) {
+        var destFile = './test/temp/nonexistent1/test1copy.css';
+        var copytest = cprf.copy('./test/fixtures/test1.css', destFile, function(err) {
+            assert.ok(path.existsSync(destFile), 'Non existent destination and file are created.');
+        });
+
+        beforeExit(function() {
+           path.exists(destFile, function(exists) {
+                fs.unlinkSync(destFile);
+           });
+        });
+    },
+
+    'copy with non existent destination directory creates destination directories' : function(beforeExit, assert) {
+        var destFile = './test/temp/nonexistent2';
+        var copytest = cprf.copy('./test/fixtures/test1.css', destFile, function(err) {
+            assert.ok(path.existsSync(destFile), 'Non existent destination directory is created.');
+        });
+
+        beforeExit(function() {
+           path.exists(destFile, function(exists) {
+                fs.unlinkSync(destFile);
+           });
+        });
+    },
+
+    'copy without callback does not emit error' : function(beforeExit, assert) {
+        var didThrow = false;
+        try {
+            var destFile = './test/temp/test1copy.css';
+            var copytest = cprf.copy('./test/fixtures/test1.css', destFile);
+        } catch (e) {
+            didThrow = true;
+        } finally {
+            assert.ok(!didThrow);
+        }
+        beforeExit(function() {
+           path.exists(destFile, function(exists) {
+                fs.unlinkSync(destFile);
+           });
+        });
+    },
+
+    'smoke test cprf' : function(beforeExit, assert) {
+        cprf.cprf(['./test/fixtures/test1.css'], './test/temp', function(){
+
+        });
+
+        beforeExit(function() {
+           path.exists('./test/temp/test1.css', function(exists) {
+                fs.unlinkSync('./test/temp/test1.css');
+           });
+        });
+    },
+
+    'test cprf can handle wildcards' : function(beforeExit, assert) {
+        cprf.cprf(['./test/fixtures/*'], './test/temp/test_wildcard1', function(){
+
+        });
+
+        beforeExit(function() {
+           path.exists('./test/temp/test_wildcard1', function(exists) {
+                fs.unlinkSync('./test/temp/test_wildcard1');
+           });
+        });
+    },
+
+    'test cprf can exclude named files' : function(beforeExit, assert) {
+        cprf.cprf(['./test/fixtures/*'], './test/temp/test_wildcard2', function(){
+            path.exists('./test/temp/test_wildcard2/test1.js', function(exists) {
+                assert.ok(!exists);
+            });
+        }, { excludes : ['test1.js'] });
+
+        beforeExit(function() {
+           path.exists('./test/temp/test_wildcard2', function(exists) {
+                fs.unlinkSync('./test/temp/test_wildcard2');
+           });
+        });
+    },
+
+    'smoke test cprf recursive copy' : function(beforeExit, assert) {
+        cprf.cprf(['./test/fixtures'], './test/temp', function(){
+
+        });
+
+        beforeExit(function() {
+           path.exists('./test/temp/fixtures', function(exists) {
+                fs.unlinkSync('./test/temp/fixtures');
+           });
+        });
     }
 }
-
-//var operation = cprf(['../examples/*'], '../examples_copy', function(err, results) {
-//    console.log('Finished copying');
-//}, {
-//   recursive : true,
-//   excludes : []
-//});
-//
-//operation.on('warning', function(err) {
-//    console.log('warning: ' + err);
-//});
-//
-//operation.on('globMatched', function(item, matches) {
-//   console.log('expanded glob: ' + item + ' matches: ' + matches);
-//});
-//
-//operation.on('parseDone', function() {
-//   console.log('parsing complete');
-//});
-//
-//operation.on('traversed', function(dir) {
-//   console.log('traversed into: ' + dir);
-//});
-//
-//operation.on('excluded', function(item) {
-//   console.log('excluded: ' + item);
-//});
-//
-//operation.on('fileStart', function(src, dest) {
-//   console.log('started: ' + src + ' ' + dest);
-//});
-//
-//operation.on('fileComplete', function(src, dest) {
-//   console.log('completed: ' + src + ' ' + dest);
-//});
-//
-//operation.on('operationCount', function(n) {
-//   console.log(n + ' operation(s) pending');
-//});
-//
-//operation.on('complete', function(){
-//   console.log('batch done!');
-//});
--- test/queue.test.js
+++ test/queue.test.js
@@ -1,5 +1,13 @@
-module.exports = {
-    'test cprf' : function(beforeExit, assert) {
+var assert = require('assert'),
+    queue = require('queue'),
+    buildy = require('buildy'),
+    fs = require('fs'),
+    path = require('path');
 
+module.exports = {
+    'smoke test single task queue' : function(beforeExit, assert) {
+        var q = new queue.Queue('test1');
+        q.task('log');
+        q.run(new buildy.Buildy());
     }
 }
