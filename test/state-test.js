586fedd5c797f5fa4eb5bfd9f62d8553f92c310c
mosen <mosen@github.com>
2012-06-04 21:49:19 +1000

    New state object passes mocha tests.

--- test/state-test.js
+++ test/state-test.js
@@ -3,16 +3,67 @@ var State = require('../lib/buildy/state.js');
 var should = require('should');
 
 describe('State', function() {
-    describe('constructing a state', function() {
-        it('should accept a string value as the only parameter', function() {
-            var testState = new State('String contents');
+
+    var state = new State('String contents');
+
+    describe('when constructing a state with a string', function() {
+
+        it('should have a type equal to State.TYPES.STRING', function() {
+            state.type().should.equal(State.TYPES.STRING);
         });
 
-        it('should have a type of State.TYPES.STRING when the type parameter is omitted', function() {
-            var testState = new State('String contents');
-            testState.type().should.equal(State.TYPES.STRING);
+        it('should have a length of 1', function() {
+            state.length().should.equal(1);
+        });
+
+        it('should contain an object with a single property', function() {
+            Object.keys(state.value()).should.have.lengthOf(1);
+        });
+
+        it('should contain a single value equal to "String contents"', function() {
+            state.value()[0].should.equal('String contents');
         });
     });
+});
 
+describe('State', function() {
 
+    var state = new State([
+        'file1.js',
+        'file2.js',
+        'file3.js'
+    ], State.TYPES.FILE);
+
+    describe('when constructing a state with an array of filenames', function() {
+
+        it('should have a type equal to State.TYPES.FILE', function() {
+            state.type().should.equal(State.TYPES.FILE);
+        });
+
+        it('should have a length of 3', function() {
+            state.length().should.equal(3);
+        });
+
+        it('should contain an object with three unique properties', function() {
+            var values = state.value();
+
+            values[0].should.not.equal(values[1]);
+            values[0].should.not.equal(values[2]);
+            values[1].should.not.equal(values[2]);
+        });
+
+        it('should give values of [file1.js, file2.js, file3.js]', function() {
+            var values = state.value();
+
+            values.should.include('file1.js');
+            values.should.include('file2.js');
+            values.should.include('file3.js');
+        });
+
+        it('should populate the metadata with the filenames given', function() {
+            state.meta('file1.js').should.include({ filename: 'file1.js', encoding: 'utf8' });
+            state.meta('file2.js').should.include({ filename: 'file2.js', encoding: 'utf8' });
+            state.meta('file3.js').should.include({ filename: 'file3.js', encoding: 'utf8' });
+        });
+    });
 });
