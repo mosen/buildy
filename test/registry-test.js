c40159b36fd424df67c1e763b9063788ae1b6565
mosen <mosen@github.com>
2012-07-10 22:00:56 +1000

    Registry skips tasks without .js suffix when loading from a directory. Registry.tasks() returns an array of tasks available. Registry.task(name) now returns an object containing acceptable types as keys, with their values being the callback for that type. Trimmed some code down that was too verbose. Registry mocha tests updated.

--- test/registry-test.js
+++ test/registry-test.js
@@ -9,8 +9,27 @@ describe('registry:', function() {
     describe('when loading a single task "concat"', function() {
         it('should contain a tasks object with a "concat" property', function() {
             var r = new Registry({ autoload: false });
-            r.load(__dirname + '/../lib/buildy/tasks/concat.js');
+            r.load(__dirname + '/../lib/buildy/tasks/concat.js'); // TODO: stub task
             r._tasks.should.have.property('concat');
         });
     });
+
+    describe('when loading a directory containing tasks', function() {
+        it('should contain more than zero tasks', function() {
+            var r = new Registry();
+            r.load(__dirname + '/../lib/buildy/tasks');
+            Object.keys(r._tasks).should.not.be.empty;
+        });
+    });
+
+    describe('when getting a task by name', function() {
+        it('should return an object with accepted types as properties', function() {
+            var r = new Registry({ autoload: true });
+            var concat_task = r.task('concat');
+            Object.keys(concat_task).forEach(function(acceptedType) {
+                ['STRING','FILE'].should.include(acceptedType);
+            });
+        });
+    });
+
 });
