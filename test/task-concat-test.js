b48d36f9933d027cb0d7c0f959820b4bb814850d
Mosen <mosen1@gmail.com>
2012-08-08 18:41:23 +1000

    Converted concat task to new format. Concat task no longer supports a 'dest' parameter, you must chain a write task after it (this is to keep separation of i/o from tasks that don't need to deal with filesystem or streams). State now supports reset() to clear the entire collection. State now supports readSync() to ensure synchronous read of an item in the collection. Reinstated concat task tests.

--- test/task-concat-test.js
+++ test/task-concat-test.js
@@ -8,49 +8,61 @@ var fixtures = require('./fixtures.js');
 
 describe('task:concat ', function() {
 
-//    describe('when run with a state that contains 2 strings', function() {
-//        it('should only contain one value', function(done) {
-//
-//            var q = new Queue('task:concat', {
-//                state: new State(['foo', 'bar'], 'STRING', {})
-//            });
-//
-//            q.task('concat').run();
-//            q.state.length().should.eql(1);
-//            done();
-//        });
-//
-//        it('should contain those strings concatenated', function(done) {
-//            var q = new Queue('task:concat', {
-//                state: new State(['foo', 'bar'], 'STRING', {})
-//            });
-//
-//            q.task('concat').run();
-//            q.state.value().should.eql('foobar');
-//            done();
-//        });
-//    });
-//
-//    describe('when run with two files', function() {
-//        it('should only contain one value', function(done) {
-//
-//            var q = new Queue('task:concat', {
-//                state: new State(['foo', 'bar'], 'STRING', {})
-//            });
-//
-//            q.task('concat').run();
-//            q.state.length().should.eql(1);
-//            done();
-//        });
+    describe('when run with a state that contains 2 strings', function() {
+        it('should only contain one value', function(done) {
+
+            var q = new Queue('task:concat', {
+                state: new State([
+                    { name: 'str1', string: 'ab' },
+                    { name: 'str2', string: 'cd' }
+                ])
+            });
+
+            q.task('concat').run();
+            q.state.length().should.eql(1);
+            done();
+        });
+
+        it('should contain those strings concatenated', function(done) {
+            var q = new Queue('task:concat', {
+                state: new State([
+                    { name: 'str1', string: 'ab' },
+                    { name: 'str2', string: 'cd' }
+                ])
+            });
+
+            q.task('concat').run();
+            q.state.get('str1-str2-concat').string.should.eql('abcd');
+            done();
+        });
+    });
+
+    describe('when run with two files', function() {
+        it('should only contain one value', function(done) {
+
+            var q = new Queue('task:concat', {
+                state: new State([
+                    { name: fixtures.file, file: fixtures.file },
+                    { name: fixtures.file, file: fixtures.file }
+                ])
+            });
+
+            q.task('concat').run();
+            q.state.length().should.eql(1);
+            done();
+        });
 
 //        it('should contain those files concatenated', function(done) {
 //            var q = new Queue('task:concat', {
-//                state: new State([fixtures.file, fixtures.file], 'FILE', {})
+//                state: new State([
+//                    { name: fixtures.file, file: fixtures.file },
+//                    { name: fixtures.file, file: fixtures.file }
+//                ])
 //            });
 //
 //            q.task('concat').run();
-//            q.state.value().should.eql('foobar');
+//            q.state.get(fixtures.file + '-' + fixtures.file + '-concat').string.should.eql('foobar');
 //            done();
 //        });
-//    });
+    });
 });
